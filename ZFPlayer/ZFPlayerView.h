540d52cdc207517a74b09a31fac33c02c762ae44
wsheng <kingwish@126.com>
2018-03-30 10:20:44 +0800

    Update ZFPlayerView.h
    修改

--- ZFPlayer/ZFPlayerView.h
+++ ZFPlayer/ZFPlayerView.h
@@ -79,6 +79,9 @@ typedef NS_ENUM(NSInteger, ZFPlayerState) {
 /** player在栈上，即此时push或者模态了新控制器 */
 @property (nonatomic, assign) BOOL                    playerPushedOrPresented;
 
+/** 是否为全屏 */
+@property (nonatomic, assign) BOOL                   isFullScreen;
+
 /**
  *  单例，用于列表cell上多个视频
  *
